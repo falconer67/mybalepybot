3c591c93bfa0b2c451fa511b7a5682e1e995a6fc
falconer67 <141686355+falconer67@users.noreply.github.com>
2024-11-17 18:56:59 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,37 @@
-from bale import Bot, Update, Message
-import asyncio
+from balethon import Client
 
-client = Bot(token="1353433662:gkyMC4m7CKzWbcg3d6FIqST15ZIxoOXt9BXUPObU")
+bot = Client("1353433662:gkyMC4m7CKzWbcg3d6FIqST15ZIxoOXt9BXUPObU")
 
-@client.event
-async def on_ready():
-    print(client.user, "is Ready!")
+replies = {
+    "hello": "Hi",
+    "hi": "Hey!",
+    "how are you": "Fine thanks! And You?",
+    "bot": "Yes, how can I help you?",
+    "haha": "What's so funny?",
+    "bye": "Good bye!"
+}
 
-@client.event
-async def on_update(update: Update):
-    print(update)
 
-@client.event
-async def on_message(message: Message):
-    await message.reply(text="Hi!")
+@bot.on_command()
+async def start(*, message):
+    texts = "\n".join(replies)
+    await message.reply(
+        f"Send one of the following texts to me and I will reply:\n{texts}"
+    )
 
-async def main():
-    while True:
-        await client.poll_updates()
-        await asyncio.sleep(3)  
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+@bot.on_command()
+async def learn(text, reply, *, message):
+    replies[text] = reply
+
+    await message.reply(
+        f"I got it\nWhen someone says {text}, I say {reply}"
+    )
+
+
+@bot.on_message(lambda client, event: event.text in replies)
+async def reply_to_text(message):
+    await message.reply(replies[message.text])
+
+
+bot.run()
